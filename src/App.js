d061b32f949cc24497972569da8ff051fcb37244
BoykoLena <boykolena15@gmail.com>
2022-07-23 23:39:04 +0300

    created bag

--- src/App.js
+++ src/App.js
@@ -1,6 +1,68 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div className="overlay">
+        <div className="drawer">
+          <h2 className="mb-30">Bag</h2>
+
+          <div className="items">
+            <div className="cartItem d-flex align-center justify-between mb-20">
+              <img
+                width={90}
+                height={80}
+                src="img/sneakers/1.png"
+                alt="sneakers"
+              />
+              <div>
+                <p>Nike 1</p>
+                <b>159 $</b>
+              </div>
+              <div>
+                <button className="button">
+                  <img
+                    width={15}
+                    height={15}
+                    src="img/delete.png"
+                    alt="delete"
+                  />
+                </button>
+              </div>
+            </div>
+
+            <div className="cartItem d-flex align-center justify-between mb-20">
+              <img
+                width={90}
+                height={80}
+                src="img/sneakers/2.png"
+                alt="sneakers"
+              />
+              <div>
+                <p>Nike 2</p>
+                <b>199 $</b>
+              </div>
+              <div>
+                <button className="button">
+                  <img
+                    width={15}
+                    height={15}
+                    src="img/delete.png"
+                    alt="delete"
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="cartTotalBlock mb-30">
+            <span>Summary</span>
+            <div></div>
+            <b>1205 $</b>
+          </div>
+
+          <button className="orderBtn">Order</button>
+        </div>
+      </div>
+
       <header className="d-flex justify-between align-center p-40">
         <div className="d-flex align-center">
           <img width={80} height={80} src="/img/logo.png" />
@@ -21,10 +83,24 @@ function App() {
         </ul>
       </header>
       <div className="content p-40">
-        <h1 className="mb-40">All sneakers</h1>
+        <div className="d-flex align-center justify-between mb-40">
+          <h1>All sneakers</h1>
+          <div className="d-flex align-center search-block ">
+            <img width={25} height={25} src="/img/search.png" alt="search" />
+            <input placeholder="search..." />
+          </div>
+        </div>
 
         <div className="d-flex flex-wrap">
           <div className="card">
+            <button className="button">
+              <img
+                width={20}
+                height={20}
+                src="img/heart-unliked.png"
+                alt="unliked"
+              />
+            </button>
             <img
               width={135}
               height={115}
@@ -44,6 +120,14 @@ function App() {
           </div>
 
           <div className="card">
+            <button className="button">
+              <img
+                width={20}
+                height={20}
+                src="img/heart-unliked.png"
+                alt="unliked"
+              />
+            </button>
             <img
               width={135}
               height={115}
@@ -63,6 +147,14 @@ function App() {
           </div>
 
           <div className="card">
+            <button className="button">
+              <img
+                width={20}
+                height={20}
+                src="img/heart-unliked.png"
+                alt="unliked"
+              />
+            </button>
             <img
               width={135}
               height={115}
@@ -82,6 +174,14 @@ function App() {
           </div>
 
           <div className="card">
+            <button className="button">
+              <img
+                width={20}
+                height={20}
+                src="img/heart-unliked.png"
+                alt="unliked"
+              />
+            </button>
             <img
               width={135}
               height={115}
